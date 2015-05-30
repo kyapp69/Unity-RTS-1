ec37beeae9577cd8f73f67747d1622131c7bb596
ghackett21 <ghackett@purdue.edu>
2015-05-30 02:24:55 -0400

    began rework on movement

---
+++ RTSII/Assets/Scripts/AirMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirMovement : Movement {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override void SetDestination(Vector3 d) {
+		base.SetDestination (d);
+		corners[0] = d;
+	}
+}
---
+++ RTSII/Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public float moveSpeed = 5f;
+	public float turnSpeed = 45f;
+	public Vector3[] corners;
+
+	bool moving;
+	Vector3 destination;
+	Rigidbody rigidbody;
+	// Use this for initialization
+	protected virtual void Start () {
+		moving = false;
+		rigidbody = GetComponent<Rigidbody> ();
+		corners = new Vector3[1];
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+		//check if reached destination
+		if ((rigidbody.position - destination).magnitude <= 1f) { 
+			Stop ();
+		}
+	}
+
+	public virtual void SetDestination(Vector3 d) {
+		destination = d;
+	}
+
+	public void Stop() {
+		moving = false;
+		rigidbody.velocity = Vector3.zero;
+	}
+
+	public bool IsMoving() {
+		return moving;
+	}
+}
